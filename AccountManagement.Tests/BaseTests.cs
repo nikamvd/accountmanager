c6f229e22f5b13c1bbf6580e7dc91253c6e0c852
Vinayak Nikam <vinayak.nikam@toro.com>
2023-02-06 11:45:26 +0530

    Added unit tests for validation rule : AreAccountsSelectedRule

---
+++ AccountManagement.Tests/BaseTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+using Xunit.Abstractions;
+
+namespace AccountManagement.Tests
+{
+    public abstract class BaseTests
+    {
+        static BaseTests()
+        {
+            MockForms.Init();
+        }
+    }
+}
+
---
+++ AccountManagement.Tests/Validations/AreAccountsSelectedRuleTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+using AccountManagement.Validations;
+using AccountManagement.ViewModels;
+using AccountManagement.Models;
+
+namespace AccountManagement.Tests.Validations
+{
+	public class AreAccountsSelectedRuleTests : BaseTests
+    {
+        private readonly AreAccountsSelectedRule _validationRule;
+        private readonly ValidatableObject<MoneyTransferViewModel> _moneyTransferViewModel;
+        public AreAccountsSelectedRuleTests()
+        {
+            // Arrange
+            _validationRule = new AreAccountsSelectedRule { ValidationMessage = "Please select From and To Accounts both." };
+            _moneyTransferViewModel = new ValidatableObject<MoneyTransferViewModel>() { Value = new MoneyTransferViewModel() };
+            _moneyTransferViewModel.Validations.Add(_validationRule);
+        }
+
+        [Fact]
+        public void AreAccountsSelectedRule_ReturnsTrue_WhenBoth_AccountsAreSelected()
+        {
+            // Arrange
+            _moneyTransferViewModel.Value.FromAccount = new Account("userId", AccountType.Current, 1000);
+            _moneyTransferViewModel.Value.ToAccount = new Account("userId", AccountType.Savings, 1000);
+
+            // Act
+            var result = _moneyTransferViewModel.Validate();
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AreAccountsSelectedRule_ReturnsFalse_WhenOneOfThe_AccountsIsNotSelected()
+        {
+            // Arrange
+            _moneyTransferViewModel.Value.FromAccount = new Account("userId", AccountType.Current, 1000);
+            _moneyTransferViewModel.Value.ToAccount = null;
+
+            // Act
+            var result = _moneyTransferViewModel.Validate();
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
+
